b28c950f904dfa879f6781122179a56cad98c60b
Jamie Vullo <jvullo@protonmail.com>
2019-06-07 22:11:39 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -3,9 +3,9 @@ class Cat
   attr_accessor :mood
   attr_reader :name
 
-  def initialize(name, mood = "nervous")
+  def initialize(name, mood)
     @name = name
-    @mood = mood
+    @mood = "nervous"
    end
  
 end
