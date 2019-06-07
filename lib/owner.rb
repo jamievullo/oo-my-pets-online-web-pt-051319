6bcbe7792b324081e936f8511261856243ae08ed
Jamie Vullo <jvullo@protonmail.com>
2019-06-07 22:09:52 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,21 @@
-class Owner
-  # code goes here
+,class Owner
+  
+  attr_accessor :pets, :name  
+  
+   def initialize(name)
+    @name = name
+    @pets = {fishes: [], cats: [], dogs: []}
+  end
+  
+  def self.count
+    
+  end
+  
+  def self.all
+    @all 
+  end
+  
+  def self.reset_all
+    
+  end
 end
