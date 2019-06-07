28db8b2149c9aab5772959c5b5d38709f5c09195
Jamie Vullo <jvullo@protonmail.com>
2019-06-07 22:17:00 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -45,19 +45,19 @@ class Owner
 
   def walk_dogs
     @@pets[:dogs].each do |dog|
-      dog.mood = 'happy'
+      dog.mood = "happy"
     end
   end
 
   def play_with_cats
     @@pets[:cats].each do |cat|
-      cat.mood = 'happy'
+      cat.mood = "happy"
     end
   end
 
   def feed_fish
     @@pets[:fishes].each do |fish|
-      fish.mood = 'happy'
+      fish.mood = "happy"
     end
   end
 
